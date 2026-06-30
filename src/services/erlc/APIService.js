538cfa3a0b2b18f3a1f33f8e38b88f22da9dd20a
manbozo5235235124 <pyradex1@gmail.com>
2026-06-29 22:19:40 -0500

    Update APIService.js

--- src/services/erlc/APIService.js
+++ src/services/erlc/APIService.js
@@ -1,1 +1,16 @@
+class APIService {
 
+    async runCommand(guildId, command) {
+
+        console.log("==============");
+        console.log("Guild:", guildId);
+        console.log("ERLC Command:", command);
+        console.log("==============");
+
+        return true;
+
+    }
+
+}
+
+module.exports = APIService;
